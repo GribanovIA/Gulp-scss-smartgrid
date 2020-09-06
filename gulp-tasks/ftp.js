a665d51245964747899841e190bae49fcbbfa46f
GribanovIA <gribanovivanjob@gmail.com>
2020-09-06 16:17:16 +0400

    fix deploy

--- gulp-tasks/ftp.js
+++ gulp-tasks/ftp.js
@@ -2,14 +2,26 @@ import {ftpData} from '../ftp-config';
 import gulp from 'gulp';
 import ftp from 'vinyl-ftp';
 
-gulp.task('deploy',()=>{
-    let conn = ftp.create({
-      host: ftpData.host,
-      user: ftpData.user,
-      password: ftpData.password,
-      parallel: 10,
-    });
-
-    return gulp.src(ftpData.src, {buffer: false})
-        .pipe(conn.dest(ftpData.dist));
+let conn = ftp.create({
+  host: ftpData.host,
+  user: ftpData.user,
+  password: ftpData.password,
+  parallel: 10,
 });
+
+gulp.task('removeFileFromServer', (cb) => {
+  conn.rmdir(ftpData.dist + '**', function (err) {
+    if (err) return cb(err);
+    cb();
+  });
+});
+
+gulp.task('downloadFileToServer', () => {
+  return gulp.src(ftpData.src, {
+    buffer: false
+  })
+    .pipe(conn.dest(ftpData.dist));
+
+});
+
+gulp.task('deploy', gulp.series('removeFileFromServer', 'downloadFileToServer'));
