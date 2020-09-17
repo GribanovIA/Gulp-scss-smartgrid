32c9bbbf168e7d60680645cd250ce412894e6d2e
GribanovIA <gribanovivanjob@gmail.com>
2020-09-17 06:45:33 +0400

    add css and libraries

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //Импорты сторонник библиотек
 // https://modernizr.com/download?setclasses
 import Modernizr from './import/modernizr-custom'; //Присваиваем класс webp если браузер имеет поддержку
+import $ from 'jquery';
 
 //Импорты BEM модулей
 import "./import/modules";
