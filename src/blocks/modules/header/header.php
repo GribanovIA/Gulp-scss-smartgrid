3e75d5d6856d31058bf0a0813729691181524c3e
GribanovIA <gribanovivanjob@gmail.com>
2020-12-12 18:58:56 +0400

    add styles

--- src/blocks/modules/header/header.php
+++ src/blocks/modules/header/header.php
@@ -1,3 +1,7 @@
 <header class="header">
-  <h1>Hello world</h1>
+  <div class="header__container container">
+    <div class="header__row row">
+      <h1 class='header__title'>Hello world</h1>
+    </div>
+  </div>
 </header>
